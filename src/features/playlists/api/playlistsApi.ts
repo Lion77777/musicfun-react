a1911b7b8b93f8fbd7aa48d1ffa375a368efc214
Lion77777 <leon.avetisyan.1993@mail.ru>
2025-12-29 10:50:34 +0400

    Add access token

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -7,6 +7,11 @@ export const playlistsApi = createApi({
         baseUrl: import.meta.env.VITE_BASE_URL,
         headers: {
             'API-KEY': import.meta.env.VITE_API_KEY
+        },
+        prepareHeaders: (headers) => {
+            headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
+            
+            return headers
         }
     }),
     endpoints: (build) => ({
