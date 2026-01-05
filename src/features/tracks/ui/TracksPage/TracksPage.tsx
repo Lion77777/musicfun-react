11b172365c1e4f679515dcb91877a5cc77e73c59
Lion77777 <leon.avetisyan.1993@mail.ru>
2026-01-05 15:56:21 +0400

    Decompose TracksPage component to TracksList and TracksLoadingTrigger components

--- src/features/tracks/ui/TracksPage/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage/TracksPage.tsx
@@ -1,64 +1,19 @@
-import { useEffect, useRef } from "react"
 import { useFetchTracksInfiniteQuery } from "../../api/tracksApi"
-import s from './TracksPage.module.css'
+import { useInfiniteScroll } from "@/common/hooks"
+import { TracksList } from "./TracksList/TracksList"
+import { TracksLoadingTrigger } from "./TracksLoadingTrigger/TracksLoadingTrigger"
 
 export const TracksPage = () => {
     const { data, hasNextPage, isFetching, isFetchingNextPage, fetchNextPage } = useFetchTracksInfiniteQuery()
     const pages = data?.pages.flatMap((page) => page.data) || []
-    const observerRef = useRef<HTMLDivElement>(null)
-
-    const loadMoreHandler = () => {
-        if(hasNextPage && !isFetching) {
-            fetchNextPage()
-        }
-    }
-
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            if(entries.length > 0 && entries[0].isIntersecting) {
-                loadMoreHandler()
-            }
-        })
-
-        const currentObserverRef = observerRef.current
-
-        if(currentObserverRef) {
-            observer.observe(currentObserverRef)
-        }
-
-        return () => {
-            if(currentObserverRef) {
-            observer.unobserve(currentObserverRef)
-        }
-        }
-    }, [loadMoreHandler])
+    const {observerRef} = useInfiniteScroll({fetchNextPage, hasNextPage, isFetching})
 
     return (
         <div>
             <h1>Tracks Page</h1>
-            <div className={s.list}>
-                {pages.map(track => {
-                    const { title, user, attachments } = track.attributes
-
-                    return (
-                        <div key={track.id} className={s.item}>
-                            <div>
-                                <p>Title: {title}</p>
-                                <p>Name: {user.name}</p>
-                            </div>
-                            {attachments.length ? <audio controls src={attachments[0].url} /> : "no file"}
-                        </div>
-                    )
-                })}
-            </div>
+            <TracksList tracks={pages}/>
             {hasNextPage && (
-                <div ref={observerRef}>
-                    {isFetchingNextPage ? (
-                        <div>Loading more tracks...</div>
-                    ) : (
-                        <div style={{height: '20px'}}></div>
-                    )}
-                </div>
+                <TracksLoadingTrigger observerRef={observerRef} isFetchingNextPage={isFetchingNextPage}/>
             )}
             {!hasNextPage && pages.length > 0 && <p>Nothing more to load</p> }
         </div>
