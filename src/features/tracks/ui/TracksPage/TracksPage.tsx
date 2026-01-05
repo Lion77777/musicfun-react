fb96a38dc4738802444970a8936ca780607557c6
Lion77777 <leon.avetisyan.1993@mail.ru>
2026-01-05 15:34:43 +0400

    Implement more tracks loading logic by scrolling the page

--- src/features/tracks/ui/TracksPage/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage/TracksPage.tsx
@@ -1,15 +1,37 @@
+import { useEffect, useRef } from "react"
 import { useFetchTracksInfiniteQuery } from "../../api/tracksApi"
 import s from './TracksPage.module.css'
 
 export const TracksPage = () => {
-    const { data, hasNextPage, isFetching, isFetchingNextPage, isLoading, fetchNextPage } = useFetchTracksInfiniteQuery()
+    const { data, hasNextPage, isFetching, isFetchingNextPage, fetchNextPage } = useFetchTracksInfiniteQuery()
     const pages = data?.pages.flatMap((page) => page.data) || []
+    const observerRef = useRef<HTMLDivElement>(null)
 
     const loadMoreHandler = () => {
         if(hasNextPage && !isFetching) {
             fetchNextPage()
         }
     }
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            if(entries.length > 0 && entries[0].isIntersecting) {
+                loadMoreHandler()
+            }
+        })
+
+        const currentObserverRef = observerRef.current
+
+        if(currentObserverRef) {
+            observer.observe(currentObserverRef)
+        }
+
+        return () => {
+            if(currentObserverRef) {
+            observer.unobserve(currentObserverRef)
+        }
+        }
+    }, [loadMoreHandler])
 
     return (
         <div>
@@ -29,17 +51,16 @@ export const TracksPage = () => {
                     )
                 })}
             </div>
-            {!isLoading &&
-                <>
-                    {hasNextPage ? (
-                        <button onClick={loadMoreHandler} disabled={isFetching}>
-                            {isFetchingNextPage ? 'Loading...' : 'Load More'}
-                        </button>
+            {hasNextPage && (
+                <div ref={observerRef}>
+                    {isFetchingNextPage ? (
+                        <div>Loading more tracks...</div>
                     ) : (
-                        <p>Nothing more to load</p>
+                        <div style={{height: '20px'}}></div>
                     )}
-                </>
-            }
+                </div>
+            )}
+            {!hasNextPage && pages.length > 0 && <p>Nothing more to load</p> }
         </div>
     )
 }
